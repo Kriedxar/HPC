2c2c12c776e37066016abdcbdf626f36eb31dd38
Angel David Santa Giraldo <angel.santa.0398@gmail.com>
2020-11-20 11:52:37 -0500

    Reto 3

--- Reto_3/cellautoMPI.c
+++ Reto_3/cellautoMPI.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]){
 
 	for(int p = 0; p < numranks; p++){
 		printf("cuak\n");
-		MPI_Scatter(&street1[p*n], (n/numranks)+2, MPI_INT,scatterStreet, (n/numranks)+2, MPI_INT, 0, MPI_COMM_WORLD);
+		MPI_Scatter(&street1[p*n/numranks], (n/numranks)+2, MPI_INT,scatterStreet, (n/numranks)+2, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Barrier(MPI_COMM_WORLD);
 		for(int i = 1; i < n/numranks+1; i++){
 			if(scatterStreet[i] == 0){
