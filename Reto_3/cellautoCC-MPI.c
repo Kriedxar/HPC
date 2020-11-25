936e07c7bcb9fa7c09e421fb2b50efb81554b61c
Angel David Santa Giraldo <angel.santa.0398@gmail.com>
2020-11-24 19:09:13 -0500

    Reto 3

--- Reto_3/cellautoCC-MPI.c
+++ Reto_3/cellautoCC-MPI.c
@@ -47,6 +47,7 @@ int main(int argc, char *argv[]){
 	startTime = MPI_Wtime();
 	for(int r = 0; r < t; r++){
 		for(int p = 0; p < numranks; p++){
+			printf("%d", p);
 			MPI_Scatter(&street1[p*n/numranks], (n/numranks)+2, MPI_INT,scatterStreet, (n/numranks)+2, MPI_INT, 0, MPI_COMM_WORLD);
 			MPI_Barrier(MPI_COMM_WORLD);
 			for(int i = 1; i < n/numranks+1; i++){
