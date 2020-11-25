c2c45599e378274f41981f2624e7409e84b9a9d6
Angel David Santa Giraldo <angel.santa.0398@gmail.com>
2020-11-25 18:05:19 -0500

    Reto 3

--- Reto_3/cellautoP2P-MPI.c
+++ Reto_3/cellautoP2P-MPI.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]){
 	MPI_Scatter(&street1[rank*n/numranks], (n/numranks)+2, MPI_INT, &scatterStreet[0], (n/numranks)+2, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
 	
-	if(rank == 2){
+	if(rank == 3){
 		for(int x = 0; x < n/numranks+2; x++){
 			if(x == 0){
 				printf("\nrank: %d - ", rank);
@@ -100,7 +100,7 @@ int main(int argc, char *argv[]){
 		MPI_Barrier(MPI_COMM_WORLD);
 
 		
-		if(rank == 2){
+		if(rank == 3){
 			for(int x = 0; x < n/numranks+2; x++){
 				if(x == 0){
 					printf("\nrank: %d - ", rank);
