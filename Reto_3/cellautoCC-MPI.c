c915f82419185a334bd2e38b78a6f17e35991b77
Angel David Santa Giraldo <angel.santa.0398@gmail.com>
2020-11-24 20:12:44 -0500

    Reto 3

--- Reto_3/cellautoCC-MPI.c
+++ Reto_3/cellautoCC-MPI.c
@@ -76,7 +76,7 @@ int main(int argc, char *argv[]){
 		}
 		street2[0] = street2[n];
 		street2[n+1] = street2[1];
-		street1 = street2;
+		//street1 = street2;
 	}
 	endTime = MPI_Wtime();
 	if(rank == 0){
