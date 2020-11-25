2cab3440902ff9348cfc6140d838b1c410980f6c
Angel David Santa Giraldo <angel.santa.0398@gmail.com>
2020-11-25 11:38:02 -0500

    Reto 3

--- Reto_3/cellautoCC-MPI.c
+++ Reto_3/cellautoCC-MPI.c
@@ -34,6 +34,11 @@ int main(int argc, char *argv[]){
 	street1[0] = street1[n];
 	street1[n+1] = street1[1];
 
+	printf("t:\t");
+	for(int i = 1; i < n+1; i++){
+		printf("%d ", street1[i]);
+	}
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numranks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -83,10 +88,6 @@ int main(int argc, char *argv[]){
 
 	if(rank == 0){
 		
-		printf("t:\t");
-		for(int i = 1; i < n+1; i++){
-			printf("%d ", street1[i]);
-		}
 		printf("\n");
 		printf("t+1:\t");
 		for(int i = 1; i < n+1; i++){
