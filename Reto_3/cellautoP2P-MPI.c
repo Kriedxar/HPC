ba0ef3aa0b48d164c768332fd4f967bc406ebb8b
Angel David Santa Giraldo <angel.santa.0398@gmail.com>
2020-11-25 17:17:33 -0500

    Reto 3

--- Reto_3/cellautoP2P-MPI.c
+++ Reto_3/cellautoP2P-MPI.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]){
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	
-	if(rank == y){
+	if(rank == 0){
 		for(int x = 0; x < n/numranks+2; x++){
 			if(x == 0){
 				printf("\nrank: %d - ", rank);
